6134edd4629cbd3a1553c1108dd390794587d874
ATalemsi <mohamadtalemsi@gmail.com>
2024-07-04 11:24:44 +0100

    modification sur index style

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Modal from 'react-modal';
 import axios from 'axios';
 import { useTranslation } from 'react-i18next';
-import { Navigate ,useNavigate } from 'react-router-dom'; // Import Navigate for navigation
+import { useNavigate } from 'react-router-dom'; // Import Navigate for navigation
 
 Modal.setAppElement('#root');
 
@@ -12,7 +12,6 @@ const Header = ({ onLanguageChange }) => {
   const [permissionGranted, setPermissionGranted] = useState(false);
   const { t, i18n } = useTranslation();
   const navigate = useNavigate(); 
-
 
   const customStyles = {
     content: {
@@ -39,6 +38,8 @@ const Header = ({ onLanguageChange }) => {
           if (permissionStatus.state === 'granted') {
             setPermissionGranted(true);
             fetchLocation();
+          } else if (permissionStatus.state === 'prompt') {
+            setShowModal(true);
           } else {
             setPermissionGranted(false);
             setLocationName('Geolocation permission denied');
@@ -91,20 +92,13 @@ const Header = ({ onLanguageChange }) => {
   };
 
   const handleAllowLocation = () => {
-    setPermissionGranted(true);
     setShowModal(false);
     fetchLocation();
   };
 
   const handleDenyLocation = () => {
-    setPermissionGranted(false);
     setShowModal(false);
-  };
-
-  const handleUseCurrentLocation = () => {
-    if (!permissionGranted) {
-      return <Navigate to="/landing" replace />;
-    } 
+    setLocationName('Geolocation permission denied');
   };
 
   return (
@@ -137,7 +131,7 @@ const Header = ({ onLanguageChange }) => {
           <a className="text-dark mx-2 fs-18 top-nav-btn-cart position-relative" data-toggle="modal" data-target="#exampleModal" href="#"><i className="feather-filter" /></a>
           <a className="toggle ml-2 text-dark hc-nav-trigger hc-nav-1" href="#" role="button" aria-controls="hc-nav-1">
             <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
-              <path fillRule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+              <path fillRule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
             </svg>
           </a>
         </div>
